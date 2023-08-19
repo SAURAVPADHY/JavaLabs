3853b8855de76227e3b1f0187b134e2f8f1ac526
saurav <saurav.padhy@sonata-software.com>
2023-08-19 11:06:55 +0530

    This is 1st Commit

---
+++ Percentage.java
@@ -0,0 +1,13 @@
+public class Percentage {
+    public static void main(String[] args){
+        int math=95;
+        int physics=98;
+        int chemistry=88;
+        int biology=78;
+        int sanskrit=99;
+        int odia=70;
+        double Percentage =(math+physics+sanskrit+odia+chemistry+biology)/600d*100d;
+        System.out.println("Percentage: " +Percentage+"%");
+    }
+    
+}
---
+++ variables.java
@@ -0,0 +1,8 @@
+public class variables {
+    public static void main(String[] args){
+        int a1 = 3 , a2= 4;
+        int a3 = (a1*a2);
+        System.out.println(a3);
+    }
+    
+}
